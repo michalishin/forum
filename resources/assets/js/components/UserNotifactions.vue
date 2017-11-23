114a056d4eca19758478ad1fbd62ef8e4cbcc313
v.mikhalishin <v.mikhalishin@outlook.com>
2017-11-23 21:32:49 +0200

    notification component create; notification fetch complete;

---
+++ resources/assets/js/components/UserNotifactions.vue
@@ -0,0 +1,28 @@
+<template>
+    <li class="dropdown" v-if="notifications.length">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+            <span class="glyphicon glyphicon-bell"></span>
+        </a>
+        <ul class="dropdown-menu">
+            <li v-for="notification in notifications">
+                <!--<a href="#" v-text="notification.data.message"></a>-->
+                <a href="#"> FooBar </a>
+            </li>
+        </ul>
+    </li>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                notifications: false
+            }
+        },
+
+        created () {
+            axios.get(`/profiles/${window.App.user.name}/notifications`)
+                .then(response => this.notifications = response.data)
+        }
+    }
+</script>
