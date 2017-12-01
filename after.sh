40c2eefcbcf0a7c27499c48bd7ea6ec3bb59e2d4
vitalii <v.michalishin@outlook.com>
2017-12-02 01:21:39 +0200

    vagrant add logic

--- after.sh
+++ after.sh
@@ -4,4 +4,11 @@
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
 
+#php-dev
 sudo apt install php7.2-gd -y
+
+#laravel
+php artisan migrate --seed
+
+#node
+npm run watch &>/dev/null &
